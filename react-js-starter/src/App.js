bf06e6ef7e5a8671ff826f75365b1aac5edfc451
martyrk <admin@martyrk.design>
2022-06-21 10:16:42 -0400

    base project setup

--- react-js-starter/src/App.js
+++ react-js-starter/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./styles/App.module.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
